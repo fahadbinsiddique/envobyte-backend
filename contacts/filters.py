d015950822a7b5836a36381f431ad64200d32ae4
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-07-07 17:47:59 +0600

    feat: filtering and search logic

---
+++ contacts/filters.py
@@ -0,0 +1,21 @@
+import django_filters
+from .models import Contact
+
+
+class ContactFilter(django_filters.FilterSet):
+    
+    favorite = django_filters.BooleanFilter(field_name='is_favorite')
+    search = django_filters.CharFilter(method='filter_search')
+
+    class Meta:
+        model = Contact
+        fields = ['favorite', 'search']
+
+    def filter_search(self, queryset, name, value):
+        from django.db.models import Q
+        return queryset.filter(
+            Q(first_name__icontains=value) |
+            Q(last_name__icontains=value) |
+            Q(email__icontains=value) |
+            Q(phone__icontains=value)
+        )
